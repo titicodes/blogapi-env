b00cb5ad924c0e8fcc947aa9de66f04a9d72e9e0
titicodes <anietimfoneeffiong@gmail.com>
2023-11-15 10:46:23 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.get('/')
+
+def index():
+    return {"data":{'name':"Anietimfon"}}
+
+@app.get('/about')
+def showAboutPage():
+    return {"about":{'about page'}}
